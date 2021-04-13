a01484e8e9c75e302fc279dedab6f8e208436817
BojanCrnovcic <gunners202@gmail.com>
2021-04-13 19:55:07 +0200

    Vjezbe

---
+++ src/Chapter8_00P/BMITest.java
@@ -0,0 +1,13 @@
+package Chapter8_00P;
+
+public class BMITest {
+    public static void main(String[] args) {
+
+
+        BMI bmi = new BMI("PERO",34,49.5,170.0);
+
+        System.out.println("Ime: "+bmi.getIme()+" ; godine: "+bmi.getGodine()+" ; BMI status: "+bmi.getBMIStatus());
+
+
+    }
+}
